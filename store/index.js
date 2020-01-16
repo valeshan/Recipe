b2d4fe259e34df86ce2c53105fc77d7f7155634d
Valeshan Naidoo <valeshan@gmail.com>
2020-01-16 22:35:36 +1100

    -refactored vuex store

--- store/index.js
+++ store/index.js
@@ -13,30 +13,26 @@ export const mutations = {
 
 export const actions = {
       async  nuxtServerInit({commit, dispatch}, pContext) {
-        let args = pContext.app.router.history.current.params.id.split('&').join(',');
+        let args = pContext.app.router.history.current.params.id? pContext.app.router.history.current.params.id.split('&').join(',') : 'chicken,tomato';
        console.log('this is the route serverinit', args);
        await dispatch('getRecipes', args);
       },
       async getRecipes({commit}, ingredients){
-        let b;
+        let b = 'http://www.recipepuppy.com/api/?i='+ingredients+'&p=1';
         let recipes;
+        let config = {
+          headers: {'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'text/javascript',
+        },
+        };
 
-        if(ingredients){
-          b = 'http://www.recipepuppy.com/api/?i='+ingredients+'&p=1';
-          console.log('this is b', b);
-          let config = {
-            headers: {'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'text/javascript',
-          },
-          };
-          await axios.get(b,config).then(res=>{
-            console.log('this is res', res);
-            recipes = res.data.results;
-          }).catch(err=>{console.log('this is the error', err)});
-          
-          console.log('recipes', recipes);
-          commit('updateRecipes', recipes)
-        }
+        await axios.get(b,config).then(res=>{
+          console.log('this is res', res);
+          recipes = res.data.results;
+        }).catch(err=>{console.log('this is the error', err)});
+        
+        console.log('recipes', recipes);
+        commit('updateRecipes', recipes)
         
       }
 }
